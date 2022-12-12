f42985fd128177df103728dd8608abcb3894d1ef
cvenero <cvenero@unsa.edu.pe>
2022-12-12 20:20:22 +0000

    Hago la conexion con el servidor e intento mostrar las tablas

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,4 +1,15 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use DBI;
+my $user='alumno';
+my $password='pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.9";
+my $dbh = DBI->connect($dsn,$user,$password) or die("No se pudo conectar!");;
 
+my $sth = $dbh->prepare(SELECT Title FROM Wiki);
+
+while(my @row = $sth->fetchrow_array){
+  print "@row\n";
+}
+$dbh->disconnect;
