62a891ff05db2784f8d340aeade2261b9c259d33
cvenero <cvenero@unsa.edu.pe>
2022-12-13 05:02:32 +0000

    Generando los enlaces de los titulos pero aun en fase de prueba

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,15 +1,32 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use CGI;
 use DBI;
+
+my $q=CGI->new;
 my $user='alumno';
 my $password='pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.9";
 my $dbh = DBI->connect($dsn,$user,$password) or die("No se pudo conectar!");;
-
-my $sth = $dbh->prepare(SELECT Title FROM Wiki);
-
+my $titulo = $q->param("Title");
+print $q->header('text/html');
+my $sth = $dbh->prepare("SELECT Title FROM Wiki");
+$sth->execute();
+print "<h1>Listado</h1>";
 while(my @row = $sth->fetchrow_array){
-  print "@row\n";
+  print "<a href=''\n>@row</a>\n";
 }
-$dbh->disconnect;
+  $sth->finish;
+  $dbh->disconnect;
+
+print <<BLOCK
+<!DOCTYPE html>
+<html>
+<head>
+<title> Listado </title>
+</head>
+<body>
+</body>
+</html>
+BLOCK
