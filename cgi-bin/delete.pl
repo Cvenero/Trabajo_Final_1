5e3e8654e42f54690e64dd5d5b21d31bb02408ce
cvenero <cvenero@unsa.edu.pe>
2022-12-14 02:16:51 +0000

    Ya es posible borrar un registro

--- cgi-bin/delete.pl
+++ cgi-bin/delete.pl
@@ -3,3 +3,34 @@ use strict;
 use warnings;
 use CGI;
 use DBI;
+
+my $q=CGI->new;
+my $user='alumno';
+my $password='pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.9";
+my $dbh = DBI->connect($dsn,$user,$password) or die("No se pudo conectar!");;
+#delete.pl
+my $titulo = '';
+#my $titulo = $q->param('');
+print $q->header('text/html');
+my $sth = $dbh->prepare("DELETE FROM Wiki WHERE Title=?");
+
+$sth->execute($titulo);
+print "<p>Borrado con exito<p>";
+print "<a href='list.pl'>Regresar al listado</a>";
+#if(my @row = $sth->fetchrow_array){
+# print "<p>@row</p>\n";
+#}
+  $sth->finish;
+  $dbh->disconnect;
+
+print <<BLOCK
+<!DOCTYPE html>
+<html>
+<head>
+<title> Listado </title>
+</head>
+<body>
+</body>
+</html>
+BLOCK
